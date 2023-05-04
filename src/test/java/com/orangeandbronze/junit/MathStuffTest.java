6826b9b739900a2d6b9a1b2851932423d3863d04
Phiangg <sophiaquizon1102@gmail.com>
2023-05-04 16:38:01 +0800

    Added additional scenarios

--- src/test/java/com/orangeandbronze/junit/MathStuffTest.java
+++ src/test/java/com/orangeandbronze/junit/MathStuffTest.java
@@ -2,19 +2,21 @@ package com.orangeandbronze.junit;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-
 public class MathStuffTest { //The test class
     @Test
     void gcf_both_params_positive_and_same_value(){
         int x = 5;
         int y = 5;
         int expected = 5;
-        int actual = new MathStuff().gcf(x, y) + 1; //instantiate a MathStuff object and call its gcf method
+        int actual = new MathStuff().gcf(x, y); //instantiate a MathStuff object and call its gcf method
         assertEquals(expected, actual); //verify if actual is the same as expected
 
     }
 
+    void gcf_both_params_positive_w_multiple_common_factors(){
+        int x = 24;
+        int y = 18;
+        assertEquals(6, new MathStuff().gcf(x, y));
+    }
 
 }
