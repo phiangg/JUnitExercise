ebde2c8edc3fd3083f8873a7a50cee278ba0a88b
Phiangg <sophiaquizon1102@gmail.com>
2023-05-04 16:23:59 +0800

    Introduced a bug to the gcf method by adding a 1

--- src/test/java/com/orangeandbronze/junit/MathStuffTest.java
+++ src/test/java/com/orangeandbronze/junit/MathStuffTest.java
@@ -11,7 +11,7 @@ public class MathStuffTest { //The test class
         int x = 5;
         int y = 5;
         int expected = 5;
-        int actual = new MathStuff().gcf(x, y); //instantiate a MathStuff object and call its gcf method
+        int actual = new MathStuff().gcf(x, y) + 1; //instantiate a MathStuff object and call its gcf method
         assertEquals(expected, actual); //verify if actual is the same as expected
 
     }
