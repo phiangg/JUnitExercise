f4fbeb0eb5cee5754f1e0b88cace7fdae6ef2041
Phiangg <sophiaquizon1102@gmail.com>
2023-05-04 16:21:56 +0800

    Creating a Test Class

--- src/test/java/com/orangeandbronze/junit/MathStuffTest.java
+++ src/test/java/com/orangeandbronze/junit/MathStuffTest.java
@@ -1,13 +1,18 @@
 package com.orangeandbronze.junit;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class MathStuffTest { //The test class
     @Test
-    void gcf_both_paramas_positive_and_same_value(){
-
+    void gcf_both_params_positive_and_same_value(){
+        int x = 5;
+        int y = 5;
+        int expected = 5;
+        int actual = new MathStuff().gcf(x, y); //instantiate a MathStuff object and call its gcf method
+        assertEquals(expected, actual); //verify if actual is the same as expected
 
     }
 
